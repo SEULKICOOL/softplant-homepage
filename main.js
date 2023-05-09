03b6ab4b087e74d897e58e658d096c5a7707709a
유슬기 <sky@yuseulgiui-MacBookPro.local>
2023-05-09 15:37:56 +0900

    올라가자

---
+++ main.js
@@ -0,0 +1,39 @@
+
+AOS.init();
+
+
+
+//portfolio swiper
+
+var swiper = new Swiper(".mySwiper", {
+
+  autoplay: true,  
+  loop: true  
+
+
+});
+
+
+
+  window.addEventListener('scroll', function() {
+    const element = document.getElementById('topButton'); 
+    
+
+    // const elementPosition = element.getBoundingClientRect().top;
+
+    const windowHeight = window.pageYOffset;
+    
+    
+    if (400< windowHeight) {
+
+      element.style.display = 'block';
+    } else {
+
+      element.style.display = 'none'; 
+    }
+  });
+
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
