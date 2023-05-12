0a9eaca0717e62ad34fb1c3c7fbbd10c2d84a3ff
sky <sky@softplant.co.kr>
2023-05-12 12:13:33 +0900

    20230512_전체수정

--- main.js
+++ main.js
@@ -5,35 +5,58 @@ AOS.init();
 
 //portfolio swiper
 
-var swiper = new Swiper(".mySwiper", {
+const contents = document.querySelectorAll(".swiper");
+let num = 0;
+var swiper = new Swiper(".mySwiper", { 
+    loop : true,
+    slidesPerView: 1.5,
+    spaceBetween: 100,
+    centeredSlides: true,
+    autoplay: {    
+    delay: 2500, 
+    disableOnInteraction: false, // false-스와이프 후 자동 재생
+    },
+    pagination : {
+      el : ".pagination-span",
+      clickable: true
+    }
+})
 
-  autoplay: true,  
-  loop: true  
 
-
-});
-
-
+//Topbutton scroll Event
 
   window.addEventListener('scroll', function() {
     const element = document.getElementById('topButton'); 
-    //iuyiuy
-
-    // const elementPosition = element.getBoundingClientRect().top;
-// ㅅㄷㄴㅅ
     const windowHeight = window.pageYOffset;
-    
-    
-    if (400< windowHeight) {
-
-      element.style.display = 'block';
-    } else {
-
-      element.style.display = 'none'; 
-    }
+      if (400< windowHeight) {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none'; 
+      }
   });
 
   function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  // Mobile
+
+  const menuIcon = document.getElementById('mobile')
+  const menu = document.querySelector('.mobile-menu');
+
+  function checkWindowSize() {
+    if (window.innerWidth <= 600) { 
+      menuIcon.style.display = 'block';
+      menu.style.display = 'none';
+    } else {
+      menuIcon.style.display = 'none';
+      menu.style.display = 'block';
+    }
+  }
+  
+  menuIcon.addEventListener('click', function() {
+    menu.classList.toggle('open');
+  });
+
+console.log(menuIcon);
