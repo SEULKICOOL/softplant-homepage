041629ffde867369efd2e56b7f313b9a9d1b5a50
sky <sky@softplant.co.kr>
2024-02-05 16:39:07 +0900

    240205

--- main.js
+++ main.js
@@ -243,3 +243,11 @@ if (value !== null) {
 // window.addEventListener("resize", resizeSwiperHandler);
 
 // resizeSwiperHandler();
+
+//구글 애널리틱스
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BVZ7B4QJF9');
+
