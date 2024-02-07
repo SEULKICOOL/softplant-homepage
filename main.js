eab0d390f9aa182c367b9c6960ad8d788cb512b6
sky <sky@softplant.co.kr>
2024-02-07 10:36:49 +0900

    240207

--- main.js
+++ main.js
@@ -268,6 +268,13 @@ r.parentNode.insertBefore(e, r);
 })("CodenButter", "https://buttr.dev/butter.js", window, "script", document);
 window.CodenButter("boot", { siteId: "tthgejhamm", auto: true });
 
+// 구글태그매니저
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5VNDV49H');
+
 
 
 
